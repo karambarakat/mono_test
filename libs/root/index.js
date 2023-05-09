1bf070f0bd6dfe9aed661a2c3c55d8f7af2d9ac1
karam barakat <karam.barakat.99@gmail.com>
2023-05-09 10:06:56 -0700

    upload secrets

--- libs/root/index.js
+++ libs/root/index.js
@@ -3,6 +3,8 @@ const dep2 = require('lodash.debounce')
 const http = require('http')
 
 const PORT = process.env.PORT
+const SECRET = process.env.SECRET
+if (!SECRET) throw new Error('SECRET not defined')
 
 if (!PORT || typeof PORT !== 'string') {
   throw new Error('PORT not defined:', PORT)
@@ -16,6 +18,7 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, () => {
   dep2(() => {})
+  console.log('secret shhh: ' + SECRET)
   console.log('dependency: ' + depLocal())
   console.log()
   console.log(`server listening on port ${PORT}`)
