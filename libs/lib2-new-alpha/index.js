e09df6fe4c51548ffef541436554d12b975d1879
karam barakat <karam.barakat.99@gmail.com>
2023-05-08 21:04:49 -0700

    delete duplicate package

--- libs/lib2-new-alpha/index.js
+++
@@ -1,9 +0,0 @@
-const hello = require('hello-world-npm')
-const dep1 = require('lodash.debounce')
-const dep2 = require('lodash.merge')
-
-module.exports = () => {
-  dep1(() => {})
-  dep2({}, {})
-  return 'p2 v2, dep:' + hello()
-}
